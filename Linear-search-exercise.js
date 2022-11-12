b6692def3c05a50b43dd794c77f8fe9225d446e0
START9INE <claudchavez1118@gmail.com>
2022-11-12 12:59:37 -0500

    Added Bubble sort and Merge sort solutions

--- Linear-search-exercise.js
+++ Linear-search-exercise.js
@@ -1,12 +1,13 @@
 // TASK: Implement Linear search.
 
 function linearSearch(list, item) {
-    let index = -1;
-    list.forEach((element, i) => {
-	if (element === item) {
-	    index = i;
-	}
-    	
-    });
-    return index;
+	let index = -1;
+	list.forEach((element, i) => {
+		if (element === item) {
+			index = i;
+		}
+	});
+	return index;
 }
+
+LinearSearch([2, 6, 7, 90, 103], 90);
---
+++ mergesort-solution.js
@@ -0,0 +1,4 @@
+// TASK: implement mergesort!
+// protip: Split the array into halves and merge them recursively
+// protip: return once we hit an array with a single item. That is a sorted array of size 1!
+// protip: compare the arrays item by item and return the concatenated result
